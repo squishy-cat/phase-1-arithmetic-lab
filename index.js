515bdc7b878622d6b921fea1771086a05998aff4
Eve Kozina <eve.kozina@gmail.com>
2021-11-01 12:29:45 -0400

    commit tree e9b64f5baec685a6012b4be70a15860e8506c163

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let multiply, num1, num2;
+    num1 = 31;
+    num2 = 2;
+    multiply = num1 * num2;
+
+
+let random = Math.ceil(Math.random() * 10)
+
+
+let mod, num3, num4;
+    num3 = 24;
+    num4 = 5;
+    mod = num3 % num4;
+
+
+let max = Math.max(1, 10, 15, 20);
